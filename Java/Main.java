42a1089216a3bca54ca7b2d398676ecc9fb9da29
Yavuzsavan <ysavan271@gmail.com>
2022-03-04 01:46:28 +0300

    sixth commit

--- Java/Main.java
+++ Java/Main.java
@@ -4,31 +4,23 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        int r;
-        double pi = 3.14;
+        double kilo, boy, index;
 
         Scanner input = new Scanner(System.in);
 
-        r = input.nextInt();
+        System.out.print("Kilo: ");
+        kilo = input.nextDouble();
 
-        double alan = pi*r*r;
-        double cevre = 2*pi*r;
+        System.out.print("Boy(metre cinsinden-nokta ile-): ");
+        boy = input.nextDouble();
 
-        System.out.println("Acıyı giriniz: ");
-        int a = input.nextInt();
-
-        double acıAlan = (pi*(r*r)*a)/360;
-
-        System.out.println("Alan: "+alan);
-        System.out.println("Cevre: "+cevre);
-        System.out.println("Açısı a olan alan: "+acıAlan);
-
-
-
-
-
+        index = kilo / (boy*boy);
+        System.out.print("Index: "+ index);
 
         
-        
+
+
+
+
     }
 }
